6b591d5c785494c5b78d77fad39bb53244a41cd0
Krishna Gupta <krishna.gupta@intelegencia.com>
2026-01-17 00:03:52 +0530

    Initial commit - IBMS MVC project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IBMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
